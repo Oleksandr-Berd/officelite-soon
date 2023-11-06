39e0374a5bff7a755da380349acfbc10826f4729
Oleksandr-Berd <alex.berd86@gmail.com>
2023-11-06 15:02:32 +0200

    the basic markUp for the contact form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,6 +1,34 @@
+import * as Yup from "yup"
+import { useFormik } from "formik";
+
 const ContactForm = () => {
+
+const validationSchema = Yup.object().shape({
+  userName: Yup.string().required("The Name field is empty"),
+  email: Yup.string().email("Incorrect Email Address").required("The Email Address field is empty"),
+  phone: Yup.string(),
+  company: Yup.string()
+});
+
+const formik = useFormik({
+    initialValues:{
+        userName: "",
+        email: "",
+        phone: "",
+        company: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit:(values)=> console.log(values),
+    
+})
+
     return ( <div>
-        <form></form>
+        <form>
+            <input type="text" name="userName" placeholder="Name" />
+            <input type="text" name="email" placeholder="Email address" />
+            <input type="text" name="phone" placeholder="Phone Number" />
+            <input type="text" name="company" placeholder="Company"/>
+        </form>
     </div> );
 }
  
